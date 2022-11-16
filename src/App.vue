9fe64ab9e1d1c5fa753153015a51cd4b7163c360
Saeid <48330269+saeidabedi97@users.noreply.github.com>
2022-11-16 11:00:46 +0100

    random fox image generator

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import RandomFox from "./components/RandomFox.vue";
+</script>
+
+<template>
+  <div>
+    <h1>This page shows random picture of foxs</h1>
+    <CatList />
+    <Suspense>
+      <RandomFox />
+      <template #fallback>
+        <img src="https://media.tenor.com/o8m3bKTsifUAAAAC/hold-on.gif" />
+      </template>
+    </Suspense>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: green;
+  font-size: 35px;
+}
+</style>
---
+++ src/components/RandomFox.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+const response = ref({});
+
+await new Promise(async (resolve) => {
+  setTimeout(() => {
+    resolve();
+  }, 1000);
+
+  try {
+    const res = await fetch(" https://randomfox.ca/floof/");
+    console.log(res);
+    response.value = await res.json();
+  } catch (error) {
+    response.value = "error! could not reach the api";
+  }
+});
+
+console.log(response.value);
+</script>
+
+<template>
+  <img :src="response.image" alt="fox image" />
+</template>
+
+<style scoped>
+img {
+  width: 350px;
+  height: 250px;
+}
+</style>
